bc77185ea74f0792fc23865da3f4489b1416490a
student-yusuf <156783245+student-yusuf@users.noreply.github.com>
2024-03-20 23:57:35 +0530

    Add files via upload

---
+++ myweb/script.js
@@ -0,0 +1,12 @@
+document.querySelector(".hover-linkss").addEventListener("click", function(){
+    document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .close-btn").addEventListener("click", function(){
+    document.querySelector(".popup").classList.remove("active");
+});
+document.querySelector(".hover-link").addEventListener("click", function(){
+    document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .close-btn").addEventListener("click", function(){
+    document.querySelector(".popup").classList.remove("active");
+});
